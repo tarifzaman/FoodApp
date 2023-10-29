c8d9d7332895e12378b845258028ed201d62ae7f
tarifzaman <97959150+tarifzaman@users.noreply.github.com>
2023-10-29 23:20:51 +0600

    Add files via upload

---
+++ client/src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import SignUp from './SignUp'
+import Login from './Login'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './Home'
+
+function App() {
+ 
+  return (
+
+      <BrowserRouter>
+      
+        <Routes>
+
+          <Route path='/register' element={<SignUp/>}> </Route>
+          <Route path='/login' element={<Login/>}> </Route>
+          <Route path='/home' element={<Home/>}> </Route>
+
+
+        </Routes>
+
+      </BrowserRouter>
+
+
+  )
+}
+
+export default App;
